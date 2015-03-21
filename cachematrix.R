45c51f6bf42964aae3cceb3ac19c7d6fce3046b5
italocadamuro <italocadamuro@hotmail.com>
2015-03-21 16:19:01 +0000

    Added more comments and a stop instruction in setInv.

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,60 @@
 ## Put comments here that give an overall description of what your
 ## functions do:
+## makeCacheMatrix : given as parameter matrix that can be inverted, 
+##                   caches the inverted of the matrix. 
+##                   Returns : an object with interfaces: 
+##                   get, setInv, getInv
+## cacheSolve      : takes as param an object created by the procedure above 
+##                   and returns the cached Inverted matrix  
 
 ## Write a short comment describing this function
 ## Input a matrix that can be inverted
 ## output a number of functions that can be used to manipolate the passed matrix.
 ## output function: 
 ##    get() returns the passed matrix
-##    getInv() returns the Inverted matrix of x
-##    setInv(matrix that can be inverted) cache the inverted matrix of the paramter.
+##    getInv() returns the Inverted matrix of the passed matrix
+##    setInv(matrix that can be inverted) caches the inverted matrix of the parameter passed.
 ##
 makeCacheMatrix <- function(x = matrix()) {
-  m <<- NULL
-  toInv <<- x
   
+  ## CachedInvertMatrix
+  CachedInvertMatrix <<- NULL
+  
+  ## MatrixParam
+  MatrixParam <<- x
+  
+  ##    get() returns the passed matrix
   get <- function(){
-    toInv
+    ## 
+    MatrixParam
   }
   
   
+  ##    setInv(matrix that can be inverted) caches the inverted matrix of the parameter passed.
+  ##          after verifying that the passed parameter is identical to the one stored
   setInv <- function(passM) {
     ## Return a matrix that is the inverse of 'x'
     
     ##  compare if the object are identical
-    if (identical(toInv,passM)) {
-      m <<- solve(toInv)
+    if (identical(MatrixParam,passM)) {
+      CachedInvertMatrix <<- solve(MatrixParam)
     } else {
        print ('Error')
-       m
+       stop ( ' Passed Matrix is not identical with the one used to create this object' )
     }
   }
   
 
 
 
+  ##    getInv() returns the Inverted matrix of the passed matrix
+  ##       ot the value: NULL if the Inverted Matrix is not available.
   getInv <- function(){
-       m
+       CachedInvertMatrix
   }
   
+  ## this func. returns a pointer to the procedures defined above
+  ## 
   list(getInv = getInv, setInv = setInv, get = get)
 }
 
@@ -54,10 +72,11 @@ cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   cachedOriginalMatrix <- x$getInv()
   if (!is.null(cachedOriginalMatrix)){
-    ## print ("return cache")
+    print ("return cache")
     cachedOriginalMatrix
     
   } else {
+    print ("creating a cache")
     ## print (x$get())
     originalMatrix <- x$get()
     InvertedMatrix<-x$setInv(originalMatrix)
